33d9e9ad987feeb49b371685d196955ab0d23ce2
xuyong@smail.swufe.edu.cn <xuy251111>
2019-05-12 13:26:59 +0800

    add double dqn and expected n step DQN

--- RLBrain_DeepRL/DeepQLearning.py
+++ RLBrain_DeepRL/DeepQLearning.py
@@ -23,20 +23,30 @@ class DeepQLearning(_BaseDeepRL):
     learning_rate: float, 0<learning_rate<1, learning rate of evaluate network
     e_greedy: float, 0<e_greedy<1, exploration rate of agent
     reward_decay: float, 0<reward_decay<1, reward decay rate
-    replace_target=100: int, the frequency of updating the target network
-    replay_size=1000: int, size of replay memory
-    batch_size=36: int, minibatch size, number of samples sampling from replay memory
+    double: whether to use double DQN  see ref : Deep Reinforcement Learning with Double Q-learning
+    EnQ: whether to use double EnQlearning  see ref:A novel multi-stepQ-learning method to improve data efficiency for
+        deep reinforcement learning (KBS 2018)
+    n_steps: Expected n-step for EnQ-learning
+    replace_target: int, the frequency of updating the target network
+    replay_size: int, size of replay memory
+    batch_size int, minibatch size, number of samples sampling from replay memory
     output_graph=False: bool, whether output the tensorflow graph.
     """
     def __init__(self, n_features, n_actions, actions,
                  learning_rate, e_greedy, reward_decay,
-                 replace_target=100, replay_size=1000,
-                 batch_size=36, output_graph=False):
+                 double,  EnQ=False, n_steps=5,
+                 replace_target=50, replay_size=1000,
+                 batch_size=64, output_graph=False):
         super().__init__(n_features, n_actions,
                          learning_rate, e_greedy, reward_decay)
         # actions index dict(index actions dict)
         self.act_idx = {act: idx for idx, act in enumerate(actions)}
         self.idx_act = {idx: act for act, idx in self.act_idx.items()}
+        self.double = double
+        self.EnQ = EnQ
+        if EnQ:
+            self.n_steps = n_steps
+            self.org_r = []
         self.replace_target_iter = replace_target
         self.memory_size = replay_size
         # number of transactions saved since started
@@ -62,7 +72,9 @@ class DeepQLearning(_BaseDeepRL):
             if os.path.exists(log_dir):
                 for file in os.listdir(log_dir):
                     os.remove(os.path.join(log_dir, file))
-            tf.summary.FileWriter(log_dir, self.sess.graph)
+            # tf.summary.FileWriter(log_dir, self.sess.graph)
+            self.writer = tf.summary.FileWriter(log_dir, self.sess.graph)
+
         self.sess.run(tf.global_variables_initializer())
         # number of learning iter since started(used for update target networks)
         self.learn_iter = 0
@@ -73,7 +85,7 @@ class DeepQLearning(_BaseDeepRL):
         self.state_ = tf.placeholder(dtype=tf.float32, shape=[None, self.n_features, ], name='state_')
         self.action = tf.placeholder(dtype=tf.int32, shape=[None, ], name='action')
         self.q_target_value = tf.placeholder(dtype=tf.float32, shape=[None, ], name='q_target_value')
-        h1_dim = 100
+        h1_dim = 256
 
         # target network
         with tf.variable_scope('target_network'):
@@ -82,7 +94,7 @@ class DeepQLearning(_BaseDeepRL):
                                  bias_initializer=tf.constant_initializer(0),
                                  name='hidden_1'
                                  )
-            self.q_target = tf.layers.dense(h1, units=self.n_actions, activation=tf.nn.relu,
+            self.q_target = tf.layers.dense(h1, units=self.n_actions, activation=None,
                                             kernel_initializer=tf.random_normal_initializer(),
                                             bias_initializer=tf.constant_initializer(0),
                                             name='q_target'
@@ -94,7 +106,7 @@ class DeepQLearning(_BaseDeepRL):
                                  bias_initializer=tf.constant_initializer(0.),
                                  name='hidden_1'
                                  )
-            self.q_eval = tf.layers.dense(s1, units=self.n_actions, activation=tf.nn.relu,
+            self.q_eval = tf.layers.dense(s1, units=self.n_actions, activation=None,
                                           kernel_initializer=tf.random_normal_initializer(),
                                           bias_initializer=tf.constant_initializer(0.),
                                           name='q_eval')
@@ -106,21 +118,23 @@ class DeepQLearning(_BaseDeepRL):
             self.q_a_eval = tf.gather_nd(params=self.q_eval, indices=a_indices, name='q_a_eval')
             self.loss = tf.reduce_mean(
                 tf.squared_difference(self.q_a_eval, self.q_target_value, name='TD_error'))
+            tf.summary.scalar('loss', self.loss)
 
         with tf.variable_scope('train'):
             self.train = \
                 tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
+        self.merged = tf.summary.merge_all()
 
     def choose_action(self, state):
         """
         choose actions based e_greedy,
-        :param state:
+        :param state: numpy array
         :return:
         """
         state = state[np.newaxis, :].astype('float32')
         if np.random.uniform(0, 1) < 1 - self.e_greedy:
             q_a = self.sess.run(self.q_target, feed_dict={self.state_: state})
-            action_idx = np.argmax(q_a)
+            action_idx = np.argmax(np.squeeze(q_a))
         else:
             action_idx = np.random.randint(0, self.n_actions)
         action = self.idx_act[action_idx]
@@ -138,8 +152,9 @@ class DeepQLearning(_BaseDeepRL):
         if self.transactions_count >= self.memory_size:
             # update the target network
             if self.learn_iter % self.replace_target_iter:
-                print('update target network')
+                # print('update target network')
                 self.sess.run(self.target_replace_op)
+
             # sample a mini batch from replay memory
             sample = np.random.randint(0, self.memory_size, size=self.batch_size)
             sample_transactions = self.replay_memory[sample, :]
@@ -152,15 +167,41 @@ class DeepQLearning(_BaseDeepRL):
             y = sample_r
             for i, d in enumerate(sample_done):
                 if d == 0:
-                    y[i] = sample_r[i] + self.reward_decay * \
-                           np.max(self.sess.run(self.q_target,
-                                                feed_dict={self.state_: sample_s_[i, :][np.newaxis, :]}))
+                    if self.double:
+                        q_a = self.sess.run(self.q_eval, feed_dict={self.state: sample_s[i, :][np.newaxis, :]})
+                        action_max = np.argmax(np.squeeze(q_a))
+                        q_a_value = self.sess.run(self.q_target,
+                                                  feed_dict={self.state_: sample_s_[i, :][np.newaxis, :]})
+                        q_value = np.squeeze(q_a_value)[action_max]
+                    else:
+                        q_value = np.max(self.sess.run(self.q_target,
+                                         feed_dict={self.state_: sample_s_[i, :][np.newaxis, :]}))
+                    y[i] = sample_r[i] + self.reward_decay * q_value
 
             # training
             self.sess.run([self.train, self.loss], feed_dict={
                 self.state: sample_s, self.q_target_value: y, self.action: sample_a
             })
             self.learn_iter += 1
+
+            if self.learn_iter % self.replace_target_iter and self.output_graph:
+
+                res = self.sess.run(self.merged, feed_dict={self.state: sample_s,
+                                                            self.q_target_value: y,
+                                                            self.action: sample_a})
+
+                self.writer.add_summary(res, global_step=self.learn_iter)
+
+    def expected_n_step_return(self, reward, done):
+        self.org_r.append(reward)
+        if len(self.org_r) < self.n_steps:
+            exp_n_r = np.mean(self.org_r)
+        else:
+            n_step_r = self.org_r[-self.n_steps:]
+            exp_n_r = np.mean(n_step_r)
+        if done == 1:
+            self.org_r = []
+        return exp_n_r
 
     def store_transactions(self, state, action, state_, reward, done):
         """
@@ -176,29 +217,42 @@ class DeepQLearning(_BaseDeepRL):
         :return:
         """
         done_ = 1 if done else 0
+        if self.EnQ:
+            reward = self.expected_n_step_return(reward, done_)
         index = self.transactions_count % self.memory_size
         action = self.act_idx[action]
         self.replay_memory[index, :] = np.hstack((state, state_, [action, reward, done_]))
         self.transactions_count += 1
 
+    def predict(self, state_):
+        pred = self.sess.run(self.q_target, feed_dict={self.state_: state_})
+        return pred
+
 
 if __name__ == '__main__':
-    rl_brain = DeepQLearning(n_features=10, n_actions=2, actions=['1', '2'],
+    from Environment.day_to_day_trans_flow import Environment
+    venv = Environment()
+
+    actions =(0.1 * np.arange(0, 50, dtype='float32')).tolist()
+    n_actions = len(actions)
+    rl_brain = DeepQLearning(n_features=12, n_actions=n_actions, actions=actions,
                              learning_rate=0.001, e_greedy=0.1,
-                             reward_decay=0.9, output_graph=True)
-    # help(rl_brain)
-    for i in range(1500):
-        state = np.random.randint(1, 100, size=(10, ))
-        a = rl_brain.choose_action(state)
-        r = np.random.randint(1, 5, 1)
-        state_ = np.random.randint(1, 100, size=(10, ))
-        done = np.random.randint(0, 2, 1)
-        rl_brain.store_transactions(state, a, state_, r, done)
-        if done == 1:
-            state = np.random.randint(1, 100, size=(10, ))
-        else:
-            state = state_
+                             reward_decay=0.9, output_graph=True, double=False)
+    epochs = 100
+    for epoch in range(epochs):
+        s = venv.reset()
+        total_r = 0
+        for t in range(30):
+            a = rl_brain.choose_action(s)
+            r, s_, done = venv.step(a)
+            rl_brain.store_transactions(s, a, s_, r, done)
+            s = s_
+            total_r += r
+            if done:
+                break
         rl_brain.learn()
+        if epoch % 10 == 0:
+            print('epoch: %d, total reward: %0.2f' % (epoch, total_r))
 
 
 
